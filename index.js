e26721bbc0974d8026dde019dea84464ca465803
tuplesxyz <tuplesxyz@protonmail.com>
2020-10-28 07:49:57 +0000

    create basic cli

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+console.log(`
+
+    Tuplesxyz
+
+    I’m just another random stranger on the 🌐
+    I do a bit of coding, designing and reading.
+    I also enjoy learning new things.
+
+    🐦 Twitter: https://twitter.com/tuplesxyz
+    👨‍💻 Github: https://github.com/tuplesxyz
+    📝 blog: https://tuples.xyz
+
+`);
